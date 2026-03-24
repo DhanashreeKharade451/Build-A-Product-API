0d1b32990546eb61298199c0974079a42b387e66
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-23 23:44:14 -0400

    update route  request is working fine

--- server.js
+++ server.js
@@ -14,5 +14,6 @@ connectDB();
 //routes
 app.use('/api/products', productRoutes);
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
